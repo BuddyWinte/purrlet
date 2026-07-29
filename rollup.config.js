810f3105894d4a2da104904786a4d2391aa6c2a8
buddywinte <buddywinte@gmail.com>
2026-07-29 09:38:58 -0500

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,56 @@
-import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
+const packageJson = require("./package.json");
 
-  output: [
-    {
-      file: "dist/purrlet.mjs",
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
+export default [
+  {
+    input: "src/index.ts",
 
-  plugins: [
-    resolve(),
-    typescript({
-      tsconfig: "./tsconfig.json",
-    }),
-    terser(),
-  ],
-};
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        file: "dist/purrlet.js",
+        format: "umd",
+        name: "Purrlet",
+        sourcemap: true,
+      },
+      {
+        file: "dist/purrlet.min.js",
+        format: "umd",
+        name: "Purrlet",
+        sourcemap: true,
+        plugins: [terser()],
+      },
+    ],
+
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.json",
+      }),
+    ],
+
+    external: [],
+  },
+
+  {
+    input: "dist/types/index.d.ts",
+    output: [
+      {
+        file: "dist/index.d.ts",
+        format: "esm",
+      },
+    ],
+    plugins: [dts()],
+  },
+];
