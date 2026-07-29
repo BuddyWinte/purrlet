b71ad2cc973ed70f164b62614a5123362e9b4616
buddywinte <buddywinte@gmail.com>
2026-07-29 11:44:36 -0500

    tool changes and package update

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,16 @@
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+import pkg from "./package.json" with { type: "json" };
+
+const banner = `/*!
+ * Purrlet v${pkg.version}
+ *
+ * Created by BuddyWinte and pawsome contributors
+ * https://github.com/BuddyWinte/Purrlet
+ *
+ * License: PolyForm Noncommercial License 1.0.0
+ */`;
 
 export default [
   {
@@ -9,11 +20,17 @@ export default [
       file: "dist/purrlet.mjs",
       format: "es",
       sourcemap: true,
+      banner,
     },
 
     plugins: [
       typescript({
         tsconfig: "./tsconfig.json",
+      }),
+      terser({
+        format: {
+          comments: /^!/,
+        },
       }),
     ],
   },
--- src/tools/eraser.ts
+++ src/tools/eraser.ts
@@ -1,3 +1,16 @@
+/**
+ * Purrlet v2.0.0
+ *
+ * Please read the CONTRIBUTING.md file for our standards on code style and contribution.
+ *
+ * @author BuddyWinte (@buddywinte)
+ * @contributors
+ * - BuddyWinte (@buddywinte) - Initial implementation
+ *
+ * @since v0.9.0
+ */
+"use strict";
+
 import type { Tool, ToolInstance, PurrletPointer } from "../types";
 import type { Renderer } from "../core/renderer";
 
